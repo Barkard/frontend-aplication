dc2f55c7c8ff2703d3c98a20495a51af52c1401b
Leon Pineda <juniorpinedafigueroa@gmail.com>
2025-02-04 01:30:01 -0400

    second commit

--- fetch.js
+++ fetch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3001';
 
-// USERS
+// USERS as
 export const fetchUsers = async () => {
   try {
     const response = await axios.get(`${API_URL}/users`);
